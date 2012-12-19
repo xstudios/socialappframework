f41b5f9630e7650c1474d7c15e397467433610a4
Tim Santor <tsantor@xstudiosinc.com>
2012-12-19 13:22:05 -0500

    Added Logout Route config

--- php/socialappframework/config/config.php
+++ php/socialappframework/config/config.php
@@ -13,37 +13,37 @@
 // ------------------------------------------------------------------------
 // SAF CONFIG
 // ------------------------------------------------------------------------
-// set app type (tab, canvas or facebook connect)
+// Set app type (tab, canvas or facebook connect)
 SAF_Config::setAppType(SAF_Config::APP_TYPE_FACEBOOK_CONNECT);
 
 // ------------------------------------------------------------------------
 // FACEBOOK CONFIG SETTINGS
 // ------------------------------------------------------------------------
-// app id & secret
+// App ID and secret
 SAF_Config::setAppID('APP_ID');
 SAF_Config::setAppSecret('APP_SECRET');
 SAF_Config::setFileUpload(false);
 
-// app name
+// App name
 SAF_Config::setAppName('APP_NAME');
 
-// app namespace (must match the one on your Facebook app's settings)
-// only needed if you are developing a canvas app
+// App namespace (must match the one on your Facebook app's settings).
+// Only needed if you are developing a canvas app
 SAF_Config::setAppNamespace('APP_NAMESPACE');
 
-// admin id(s) - comma delimited
+// Admin id(s) - comma delimited
 SAF_Config::setAdmins('743687287');
 
-// app developer id(s) - comma delimited
+// App developer id(s) - comma delimited
 SAF_Config::setDevelopers('743687287');
 
 // ------------------------------------------------------------------------
 // FACEBOOK GRAPH FIELDS
 // ------------------------------------------------------------------------
-// fields we want returned from the graph API for users and pages (comma delimited)
-// if you just want all the default fields set the parameter to an empty string
-// user fields: https://developers.facebook.com/docs/reference/api/user/
-// page fields: https://developers.facebook.com/docs/reference/api/page/
+// Fields we want returned from the graph API for users and pages (comma delimited).
+// If you just want all the default fields set the parameter to an empty string
+// User fields: https://developers.facebook.com/docs/reference/api/user/
+// Page fields: https://developers.facebook.com/docs/reference/api/page/
 SAF_Config::setGraphUserFields('id, name, first_name, last_name, gender, username, email, link, picture, website');
 SAF_Config::setGraphPageFields('id, name, link, picture, website, is_published, likes');
 
@@ -52,37 +52,47 @@ SAF_Config::setGraphPageFields('id, name, link, picture, website, is_published, 
 // ------------------------------------------------------------------------
 // https://developers.facebook.com/docs/reference/api/permissions/
 
-// extended permissions - comma delimited
+// Extended permissions - comma delimited
 SAF_Config::setExtendedPerms('');
 
-// required extended permissions for admin(s) - comma delimited
-// usually the extended permissions above, plus additional permissions (eg - manage_pages)
+// Required extended permissions for admin(s) - comma delimited
+// Usually the extended permissions above, plus additional permissions (eg - manage_pages)
 SAF_Config::setExtendedPermsAdmin('');
 
-// auto request permissions upon tab and/or canvas app load
+// Auto request permissions upon tab and/or canvas app load
 SAF_Config::setAutoRequestPermsTab(false);
 SAF_Config::setAutoRequestPermsCanvas(false);
 
-// force admin to auth app upon tab and/or canvas app load
+// Force admin to auth app upon tab and/or canvas app load
 SAF_Config::setAutoRequestPermsAdmin(false);
 
 // ------------------------------------------------------------------------
 // FACEBOOK SIGNED REQUEST
 // ------------------------------------------------------------------------
-// there is no way to determine our fan page tab link without page info
+// There is no way to determine our fan page tab link without page info
 // supplied from the signed request so we need to assign a fallback value
 SAF_Config::setFanPageHash('FAN_PAGE_HASH');
 
-// force the user to view tab and canvas app in Facebook
+// Force the user to view tab and canvas app in Facebook
 SAF_Config::setForceFacebookView(true);
 
 // ------------------------------------------------------------------------
 // SESSION REDIRECT - 3RD PARTY COOKIE BLOCK WORK-AROUND
 // ------------------------------------------------------------------------
-// force the app to redirect to the base url (where the app is hosted) before
+// Force the app to redirect to the base url (where the app is hosted) before
 // redirecting back to the app tab (allows session to be started on the app domain)
 // this is used as a work around for browsers which block 3rd party cookies
 SAF_Config::setForceSessionRedirect(false);
+
+// ------------------------------------------------------------------------
+// LOGOUT ROUTE
+// ------------------------------------------------------------------------
+// Used as the 'next' parameter in the Facebook SDK's getLogoutUrl() method.
+// Once logged out out of facebook (using a logout link in your app), the user would
+// be redirected here. This is where you would clear your session, etc. to properly
+// log them out of the application. This value gets appended to the base URL.
+// Example: http://domain.com/logout
+SAF_Config::setLogoutRoute('logout');
 
 // ------------------------------------------------------------------------
 
