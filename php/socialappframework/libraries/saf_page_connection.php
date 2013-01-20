5e788459d5e83f43fde1266999f888e3bbed8996
Tim Santor <tsantor@xstudiosinc.com>
2013-01-20 12:47:20 -0500

    Put methods in alphabetical order

--- php/socialappframework/libraries/saf_page_connection.php
+++ php/socialappframework/libraries/saf_page_connection.php
@@ -76,90 +76,15 @@ class SAF_Page_Connection {
     // ------------------------------------------------------------------------
 
     /**
-     * Get the page's wall
+     * Returns a list of the page's admins.
      *
      * @access    public
-     * @return    array  of Post objects
+     * @return    array  of objects containing id, name
      */
-    public function getFeed() {
-        return $this->getConnection('feed');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Get the page's profile picture
-     *
-     * @access    public
-     * @param     string  $type  square, small, normal, large
-     * @return    string  URL of the page's profile picture
-     */
-    public function getPicture($type='square') {
-        return $this->getConnection('picture', array(
-            'type' => $type
-        ));
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Get the page's settings
-     *
-     * @access    public
-     * @return    array  of objects containing setting and value fields
-     */
-    public function getSettings() {
-        return $this->getConnection('settings', array(
+    public function getAdmins() {
+        return $this->getConnection('admins', array(
             'access_token' => $this->_getPageAccessToken()
         ));
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns the photos, videos, and posts in which the Page has been tagged.
-     *
-     * @access    public
-     * @return    array  of Photo, Video or Post objects
-     */
-    public function getTagged() {
-        return $this->getConnection('tagged');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns the the page's posted links.
-     *
-     * @access    public
-     * @return    array  of link objects
-     */
-    public function getLinks() {
-        return $this->getConnection('links');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns the page's uploaded photos.
-     *
-     * @access    public
-     * @return    array  of Photo objects
-     */
-    public function getPhotos() {
-        return $this->getConnection('photos');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns the groups to which the page belongs.
-     *
-     * @access    public
-     * @return    array  array containing group id, version, name & unread fields
-     */
-    public function getGroups() {
-        return $this->getConnection('groups');
     }
 
     // ------------------------------------------------------------------------
@@ -177,25 +102,134 @@ class SAF_Page_Connection {
     // ------------------------------------------------------------------------
 
     /**
-     * Returns the page's status updates.
+     * Returns a list of users blocked from the page.
      *
      * @access    public
-     * @return    array  of status message objects
+     * @return    array  of objects containing id, name
      */
-    public function getStatuses() {
-        return $this->getConnection('statuses');
+    public function getBlocked() {
+        return $this->getConnection('blocked', array(
+            'access_token' => $this->_getPageAccessToken()
+        ));
     }
 
     // ------------------------------------------------------------------------
 
     /**
-     * Returns the videos the page has uploaded.
+     * Returns the checkins made to this place page by the
+     * current user, and friends of the current user.
+     *
+     * Permissions: user_checkins or friends_checkins
      *
      * @access    public
-     * @return    array  of video objects
+     * @return    array  of checkin objects
      */
-    public function getVideos() {
-        return $this->getConnection('videos');
+    public function getCheckins() {
+        return $this->getConnection('checkins');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns a list of the page's conversations.
+     *
+     * Permissions: Page admin access token with read_mailbox permission
+     *
+     * @access    public
+     * @return    array  of checkin objects
+     */
+    public function getConversations() {
+        return $this->getConnection('conversations', array(
+            'access_token' => $this->_getPageAccessToken()
+        ));
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns the events the page is attending.
+     *
+     * @access    public
+     * @return    array  of event objects
+     */
+    public function getEvents() {
+        return $this->getConnection('events');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the page's wall
+     *
+     * @access    public
+     * @return    array  of Post objects
+     */
+    public function getFeed() {
+        return $this->getConnection('feed');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns information of all children pages.
+     *
+     * @access    public
+     * @return    array  of JSON objects
+     */
+    public function getGlobalBrandChildren() {
+        return $this->getConnection('global_brand_children');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns the groups to which the page belongs.
+     *
+     * @access    public
+     * @return    array  array containing group id, version, name & unread fields
+     */
+    public function getGroups() {
+        return $this->getConnection('groups');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the page's Insights data
+     *
+     * Permissions: read_insights permission
+     *
+     * @access    public
+     * @return    array  of insights objects
+     */
+    public function getInsights() {
+        return $this->getConnection('insights', array(
+            'access_token' => $this->_getPageAccessToken()
+        ));
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns the the page's posted links.
+     *
+     * @access    public
+     * @return    array  of link objects
+     */
+    public function getLinks() {
+        return $this->getConnection('links');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns a list of the page's milestones.
+     *
+     * @access    public
+     * @return    array  of milestone objects
+     */
+    public function getMilestones() {
+        return $this->getConnection('milestones');
     }
 
     // ------------------------------------------------------------------------
@@ -208,6 +242,33 @@ class SAF_Page_Connection {
      */
     public function getNotes() {
         return $this->getConnection('notes');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns the page's uploaded photos.
+     *
+     * @access    public
+     * @return    array  of Photo objects
+     */
+    public function getPhotos() {
+        return $this->getConnection('photos');
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the page's profile picture
+     *
+     * @access    public
+     * @param     string  $type  square, small, normal, large
+     * @return    string  URL of the page's profile picture
+     */
+    public function getPicture($type='square') {
+        return $this->getConnection('picture', array(
+            'type' => $type
+        ));
     }
 
     // ------------------------------------------------------------------------
@@ -250,40 +311,13 @@ class SAF_Page_Connection {
     // ------------------------------------------------------------------------
 
     /**
-     * Returns the events the page is attending.
+     * Get the page's settings
      *
      * @access    public
-     * @return    array  of event objects
+     * @return    array  of objects containing setting and value fields
      */
-    public function getEvents() {
-        return $this->getConnection('events');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns the checkins made to this place page by the
-     * current user, and friends of the current user.
-     *
-     * Permissions: user_checkins or friends_checkins
-     *
-     * @access    public
-     * @return    array  of checkin objects
-     */
-    public function getCheckins() {
-        return $this->getConnection('checkins');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns a list of the page's admins.
-     *
-     * @access    public
-     * @return    array  of objects containing id, name
-     */
-    public function getAdmins() {
-        return $this->getConnection('admins', array(
+    public function getSettings() {
+        return $this->getConnection('settings', array(
             'access_token' => $this->_getPageAccessToken()
         ));
     }
@@ -291,43 +325,13 @@ class SAF_Page_Connection {
     // ------------------------------------------------------------------------
 
     /**
-     * Returns a list of the page's conversations.
-     *
-     * Permissions: Page admin access token with read_mailbox permission
+     * Returns the page's status updates.
      *
      * @access    public
-     * @return    array  of checkin objects
+     * @return    array  of status message objects
      */
-    public function getConversations() {
-        return $this->getConnection('conversations', array(
-            'access_token' => $this->_getPageAccessToken()
-        ));
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns a list of the page's milestones.
-     *
-     * @access    public
-     * @return    array  of milestone objects
-     */
-    public function getMilestones() {
-        return $this->getConnection('milestones');
-    }
-
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns a list of users blocked from the page.
-     *
-     * @access    public
-     * @return    array  of objects containing id, name
-     */
-    public function getBlocked() {
-        return $this->getConnection('blocked', array(
-            'access_token' => $this->_getPageAccessToken()
-        ));
+    public function getStatuses() {
+        return $this->getConnection('statuses');
     }
 
     // ------------------------------------------------------------------------
@@ -349,29 +353,25 @@ class SAF_Page_Connection {
     // ------------------------------------------------------------------------
 
     /**
-     * Returns information of all children pages.
+     * Returns the photos, videos, and posts in which the Page has been tagged.
      *
      * @access    public
-     * @return    array  of JSON objects
+     * @return    array  of Photo, Video or Post objects
      */
-    public function getGlobalBrandChildren() {
-        return $this->getConnection('global_brand_children');
+    public function getTagged() {
+        return $this->getConnection('tagged');
     }
 
     // ------------------------------------------------------------------------
 
     /**
-     * Get the page's Insights data
-     *
-     * Permissions: read_insights permission
+     * Returns the videos the page has uploaded.
      *
      * @access    public
-     * @return    array  of insights objects
+     * @return    array  of video objects
      */
-    public function getInsights() {
-        return $this->getConnection('insights', array(
-            'access_token' => $this->_getPageAccessToken()
-        ));
+    public function getVideos() {
+        return $this->getConnection('videos');
     }
 
     // ------------------------------------------------------------------------
