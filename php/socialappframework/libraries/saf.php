1a2ba37c4e34c3e03dd7f8ec2bea6711d33e4abd
Tim Santor <tsantor@xstudiosinc.com>
2013-01-24 18:32:38 -0500

    Added factory methods to create graph objects

--- php/socialappframework/libraries/saf.php
+++ php/socialappframework/libraries/saf.php
@@ -7,7 +7,13 @@
  * with this package. If not, see <http://socialappframework.com/license/>.
  */
 
-require_once dirname(__FILE__).'/facebook/sdk/facebook.php';
+// config
+require_once dirname(__FILE__).'/../config/config.php';
+
+// helpers
+require_once dirname(__FILE__).'/../helpers/fb_helper.php';
+
+// saf
 require_once dirname(__FILE__).'/saf_config.php';
 require_once dirname(__FILE__).'/saf_base.php';
 require_once dirname(__FILE__).'/saf_debug.php';
@@ -16,8 +22,18 @@ require_once dirname(__FILE__).'/saf_page.php';
 require_once dirname(__FILE__).'/saf_page_connection.php';
 require_once dirname(__FILE__).'/saf_user.php';
 require_once dirname(__FILE__).'/saf_user_connection.php';
-require_once dirname(__FILE__).'/../config/config.php';
-require_once dirname(__FILE__).'/../helpers/fb_helper.php';
+
+// facebook api
+require_once dirname(__FILE__).'/facebook/sdk/facebook.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_notification.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_post.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_comment.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_question.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_note.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_link.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_album.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_checkin.php';
+require_once dirname(__FILE__).'/facebook/api/publish/fb_event.php';
 
 /**
  * SAF class
@@ -59,6 +75,8 @@ class SAF extends SAF_Signed_Request {
     }
 
     // ------------------------------------------------------------------------
+    // PUBLIC METHODS
+    // ------------------------------------------------------------------------
 
     /**
      * Get instance
@@ -81,6 +99,114 @@ class SAF extends SAF_Signed_Request {
      */
     final public function __clone() {
         return false;
+    }
+
+    // ------------------------------------------------------------------------
+    // FACTORY METHODS
+    // ------------------------------------------------------------------------
+
+    /**
+     * Returns a Facebook Album
+     *
+     * @access    public
+     * @param     string  $name
+     * @return    FB_Album
+     */
+    public static function createAlbum($name) {
+        return new FB_Album($name);
+    }
+
+    /**
+     * Returns a Facebook Checkin
+     *
+     * @access    public
+     * @param     string  $place  the Place Page ID
+     * @param     string  $latitude
+     * @param     string  $longitude
+     * @return    FB_Checkin
+     */
+    public static function createCheckin($place, $latitude, $longitude) {
+        return new FB_Checkin($place, $latitude, $longitude);
+    }
+
+    /**
+     * Returns a Facebook Comment
+     *
+     * @access    public
+     * @param     string  $message
+     * @return    FB_Comment
+     */
+    public static function createComment($message) {
+        return new FB_Comment($message);
+    }
+
+    /**
+     * Returns a Facebook Event
+     *
+     * @access    public
+     * @param     string  $name        the event name
+     * @param     string  $start_time  the event start time, in ISO-8601
+     * @return    FB_Event
+     */
+    public static function createEvent($name, $start_time) {
+        return new FB_Event($name, $start_time);
+    }
+
+    /**
+     * Returns a Facebook Link
+     *
+     * @access    public
+     * @param     string  $url
+     * @return    FB_Link
+     */
+    public static function createLink($url) {
+        return new FB_Link($url);
+    }
+
+    /**
+     * Returns a Facebook Note
+     *
+     * @access    public
+     * @param     string  $subject  the subject
+     * @param     string  $message  the comment
+     * @return    FB_Note
+     */
+    public static function createNote($subject, $message) {
+        return new FB_Note($subject, $message);
+    }
+
+    /**
+     * Returns a Facebook Notification
+     *
+     * @access    public
+     * @param     string  $template  the template text
+     * @param     string  $href      the tracking data added to the url
+     * @return    FB_Notification
+     */
+    public static function createNotification($template, $href) {
+        return new FB_Notification($template, $href);
+    }
+
+    /**
+     * Returns a Facebook Post
+     *
+     * @access    public
+     * @param     string  $message
+     * @return    FB_Post
+     */
+    public static function createPost($message) {
+        return new FB_Post($message);
+    }
+
+    /**
+     * Returns a Facebook Question
+     *
+     * @access    public
+     * @param     string  $question  the text of the question
+     * @return    FB_Question
+     */
+    public static function createQuestion($question) {
+        return new FB_Question($question);
     }
 
     // ------------------------------------------------------------------------
